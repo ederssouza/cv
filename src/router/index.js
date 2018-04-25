3ba4467ce56d13de1d76817a63113c7fa9fd2ef2
Eder Sampaio <edersampaio@outlook.com.br>
2018-04-25 18:57:59 -0300

    enabled mode history

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const Page404 = () => System.import('../components/Page404')
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
